0ab58d8b360bd0d3166c21193c67e66aef1d5055
unknown <hcsinfosolutions@gmail.com>
2018-07-20 00:14:45 +0530

    Updated resources

---
+++ HCS.StaffManagement/HCS.StaffManagement/App_Start/BundleConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace HCS.StaffManagement.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundle)
+        {
+
+        }
+    }
+}
