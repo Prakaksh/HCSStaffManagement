9d382acfc6538aa7fbc3c7ff2bc84081f2ebd35a
hcsinfosolutions <hcsinfosolutions@gmail.com>
2018-08-11 20:19:26 +0530

    MasterController Added

---
+++ HCS.StaffManagement/HCS.StaffManagement/Controllers/MasterController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HCS.StaffManagement.Filter;
+using HCS.StaffManagement.Models;
+using HCS.StaffManagement.Repositories;
+
+namespace HCS.StaffManagement.Controllers
+{
+    [SessionTimeout]
+    public class MasterController : Controller
+    {
+
+        [HttpGet]
+        public JsonResult CountryStateGet(MasterCountryState objstate)
+        {
+            try
+            {
+                CountryStateContext objStateContext = new CountryStateContext();
+                IEnumerable<MasterCountryState> maritalStatuses = objStateContext.GetCountryState(objstate);
+                return Json(maritalStatuses, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+                return Json("[]");
+            }
+        }
+    }
+}
